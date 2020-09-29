a462949a939bff4e511fac02772a37b01e587c79
mdwohl <mdwohl@gmail.com>
2020-09-29 08:33:09 -0700

    cc 11

---
+++ DataStructures/src/main/LinkedList.java
@@ -0,0 +1,32 @@
+package DataStructures
+
+import org.w3c.dom.Node;
+
+public class LinkedList {
+
+    Node head;
+    String head;
+    String tail;
+
+   static class Node {
+       public String value;
+       Node next;
+
+
+       Node(String data) {
+           this.data = data;
+           this.next = null;
+           this.head = null;
+           this.tail = null;
+       }
+   }
+
+   public String insert(String data){
+       Node node = new Node(data);
+       if (this.head == null) {
+           node.next = this.head;
+           this.head = node;
+       }
+       return node.value;
+   }
+}
